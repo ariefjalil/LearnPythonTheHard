5ba9521909684a701c424140744f87efd9e8f64a
ariefjalil <30983999+ariefjalil@users.noreply.github.com>
2017-08-14 17:40:33 +0800

    multi function def

---
+++ def.py
@@ -0,0 +1,14 @@
+def myfirstpoem():
+
+print "Tired eyes"
+print "Sleepy eyes"
+print "Grump"
+print "...I need sleep"
+
+
+def mysecondpoem():
+
+print "Coffee.."
+print "Sugar or cream.."
+print "I don't drink coffee."
+
