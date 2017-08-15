142992022f2a2f08067c065939d7c45f5f7a4684
ariefjalil <30983999+ariefjalil@users.noreply.github.com>
2017-08-15 20:49:54 +0800

    defmain

--- birthday.py
+++ birthday.py
@@ -3,3 +3,16 @@ def birthdayemily():
     print("Happy Birthday to you!")
     print("Happy Birthday, dear Emily.")
     print("Happy Birthday to you!")
+
+def birthdayandre():
+    print("Happy Birthday to you!")
+    print("Happy Birthday to you!")
+    print("Happy Birthday, dear Andre.")
+    print("Happy Birthday to you!")
+
+def main():
+
+    birthdayemily()
+    birthdayandre()
+main()
+
