c25a5e7d9aa2d16c67fefcde61e4d56b2f5fd0c4
ariefjalil <30983999+ariefjalil@users.noreply.github.com>
2017-08-15 16:08:40 +0800

    raw_input return string

--- algebra.py
+++ algebra.py
@@ -1,7 +1,7 @@
 def sumProblem(x, y):
     sum = x + y
-    sentence = 'The sum {} and {} is {}.'.format(x, y, sum)
-    print sentence
+    #sentence = 'The sum {} and {} is {}.'.format(x, y, sum)
+    #print sentence
     print " The sum %r and %r is %r." % (x, y, sum)
     
 
@@ -11,7 +11,14 @@ def main():
     sumProblem(1234567890123, 53590248291058)
     a = int(input("Enter an integer: "))
     b = int(input("Enter another integer: "))
+
+
+#anything given to raw_input is return as string. e.g c =4, d=3 = '43' and not 7
+    c = raw_input("enter an integer:")
+    d = raw_input ("enter an integer again:")
     sumProblem(a,b)
+    sumProblem(c,d)
+    
 
 main()
 
