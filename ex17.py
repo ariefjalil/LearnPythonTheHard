56de59bd372e0229d8e3d5a3c297345a42d48ae1
ariefjalil <30983999+ariefjalil@users.noreply.github.com>
2017-09-25 18:53:57 +0800

    modifier

--- ex17.py
+++ ex17.py
@@ -9,11 +9,13 @@ script, from_file, to_file = argv
 print "Opening the file %s....." %from_file
 target = open(from_file, 'w') #'w' creates a file if it doesnt exits, empties if it does
 
-print"Now I'm going to ask you to write these to the file."
+print"Now I'm going to ask you to write 3 lines to the file."
 line1 = raw_input("Line 1: ")
+line2 = raw_input("Line 2: ")
+line3 = raw_input("Line 3: ")
 print" I'm going to write these to the file."
 
-target.write(line1)
+target.write("%s \n %s \n %s \n" % (line1, line2,line3))
 target.close() # .close() acts as "SAVE file"
 
 print "Copying from %s to %s" %(from_file, to_file)
---
+++ ex22.py
@@ -0,0 +1,12 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+print "What is your name?"
+name = raw_input(">")
+print "What is your age?"
+age = raw_input(">")
+
+print "Here are the days:", days
+
+print " Your name is %s and you are %s years old" %(name, age)
+
+print "This is the day %s" %days
