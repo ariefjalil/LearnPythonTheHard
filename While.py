d0dc09de304b20bde08e15a2194a72b58de0a948
ariefjalil <30983999+ariefjalil@users.noreply.github.com>
2017-10-07 11:10:26 +0800

    break

--- While.py
+++ While.py
@@ -1,4 +1,6 @@
-spam =  0
-while spam < 100:
-    print "Hello world."
-    spam = spam + 1
+while True:
+    print "Please type your name."
+    name = raw_input()
+    if name == 'your name':
+        break
+print "Thank you"
